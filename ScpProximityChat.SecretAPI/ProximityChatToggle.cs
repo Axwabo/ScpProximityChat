6147b89ae43bf7667aadada42c4470217e693500
Axwabo <axwabo@gmail.com>
2025-06-16 23:37:34 +0200

    SSSS with SecretAPI

---
+++ ScpProximityChat.SecretAPI/ProximityChatToggle.cs
@@ -0,0 +1,24 @@
+﻿using LabApi.Features.Wrappers;
+using SecretAPI.Features.UserSettings;
+using UnityEngine;
+
+namespace ScpProximityChat.SecretAPI;
+
+public sealed class ProximityChatToggle : CustomKeybindSetting
+{
+
+    public ProximityChatToggle() : base(null, "Toggle SCP Proximity Chat", KeyCode.LeftAlt, allowSpectatorTrigger: false)
+    {
+    }
+
+    protected override CustomSetting CreateDuplicate() => new ProximityChatToggle();
+
+    protected override void HandleSettingUpdate(Player player)
+    {
+        if (player.CanUseProximityChat())
+            player.ToggleProximityChat();
+    }
+
+    public override CustomHeader Header { get; } = new("Proximity Chat");
+
+}
---
+++ ScpProximityChat.SecretAPI/SecretApiProximityChat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LabApi.Loader.Features.Plugins;
+using SecretAPI.Features.UserSettings;
+
+namespace ScpProximityChat.SecretAPI;
+
+public sealed class SecretApiProximityChat : Plugin
+{
+
+    public override string Name => "SSSS Proximity Chat";
+    public override string Description => "SSSS-based proximity chat toggle";
+    public override string Author => "Axwabo";
+    public override Version Version => GetType().Assembly.GetName().Version;
+    public override Version RequiredApiVersion { get; } = new(1, 0, 0);
+
+    public override void Enable() => CustomSetting.Register(new ProximityChatToggle());
+
+    public override void Disable()
+    {
+    }
+
+}
