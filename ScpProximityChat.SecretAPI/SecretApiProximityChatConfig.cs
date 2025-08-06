6276fa30abc2bd6d4e9903f502a049135e81d0f0
Axwabo <axwabo@gmail.com>
2025-08-06 19:18:05 +0200

    add more new lines to available hint

--- ScpProximityChat.SecretAPI/SecretApiProximityChatConfig.cs
+++ ScpProximityChat.SecretAPI/SecretApiProximityChatConfig.cs
@@ -8,7 +8,7 @@ public sealed class SecretApiProximityChatConfig
 
     public bool ShowAvailableHint { get; set; } = true;
 
-    public string AvailableHint { get; set; } = "\n\n\n\nSCP Proximity Chat available.\nPress <mark=#77777755><size=0>.</size><space=0.2em><b>{0}</b><space=0.2em><size=0>.</size></mark> to toggle.";
+    public string AvailableHint { get; set; } = "\n\n\n\n\n\nSCP Proximity Chat available.\nPress <mark=#77777755><size=0>.</size><space=0.2em><b>{0}</b><space=0.2em><size=0>.</size></mark> to toggle.";
 
     [Description("Whether players can adjust the volume of others' proximity messages.")]
     public bool Personalization { get; set; } = true;
