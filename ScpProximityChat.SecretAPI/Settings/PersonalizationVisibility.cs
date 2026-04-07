02e3667027164c9b6781fe4cbdd606b146c3acff
Axwabo <axwabo@gmail.com>
2026-04-07 11:44:02 +0200

    update SecretAPI + fix PersonalizationVisibility

--- ScpProximityChat.SecretAPI/Settings/PersonalizationVisibility.cs
+++ ScpProximityChat.SecretAPI/Settings/PersonalizationVisibility.cs
@@ -5,20 +5,17 @@ internal sealed class PersonalizationVisibility : CustomTwoButtonSetting
 
     public const string Text = "Personalization Visibility";
 
-    private bool _previouslyVisible;
-
     public PersonalizationVisibility() : base(Text.GetStableHashCode(), Translation.VisibilityLabel, Translation.Shown, Translation.Hidden)
         => IsShared = SettingsRegistry.Shared;
 
-    public bool Visible => IsOptionB;
+    public bool Visible => IsOptionA;
 
     protected override CustomSetting CreateDuplicate() => new PersonalizationVisibility();
 
     protected override void HandleSettingUpdate()
     {
-        if (_previouslyVisible != Visible)
+        if (HasValueChanged)
             ResyncToOwner();
-        _previouslyVisible = Visible;
     }
 
     public override CustomHeader Header => Headers.ProximityChat;
